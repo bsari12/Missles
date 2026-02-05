ced23854e484116c66f8636cd4bc30d2c36d5b42
bsari12 <berkaysari1225@gmail.com>
2026-02-05 09:41:07 +0300

    Fix

--- Assets/Scripts/SimpleJoystick.cs
+++ Assets/Scripts/SimpleJoystick.cs
@@ -1,33 +1,71 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class SimpleJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
+    public RectTransform background;
     public RectTransform handle;
-    private RectTransform background;
-    private float radius;
     
+    [Range(0f, 2f)] 
+    public float handleRange = 1f;
+
     public Vector2 InputVector { get; private set; }
+
+    private Vector2 joystickPosition = Vector2.zero;
+    private Camera cam;
 
     void Start()
     {
-        background = GetComponent<RectTransform>();
-        radius = background.sizeDelta.x / 2f;
+        if (CanvasUpdateRegistry.IsRebuildingLayout()) 
+            Canvas.ForceUpdateCanvases();
+            
+        handleRange = 1f;
+        cam = null; 
+        
+        Canvas canvas = GetComponentInParent<Canvas>();
+        if (canvas.renderMode != RenderMode.ScreenSpaceOverlay)
+            cam = canvas.worldCamera;
+
+        Vector2 center = new Vector2(0.5f, 0.5f);
+        background.pivot = center;
+        handle.anchorMin = center;
+        handle.anchorMax = center;
+        handle.pivot = center;
+        handle.anchoredPosition = Vector2.zero;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Vector2 pos;
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(background, eventData.position, eventData.pressEventCamera, out pos))
+        Vector2 position = Vector2.zero;
+        
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            background, 
+            eventData.position, 
+            eventData.pressEventCamera, 
+            out position))
         {
-            pos = Vector2.ClampMagnitude(pos, radius);
-            handle.anchoredPosition = pos;
+            position.x = (position.x / background.sizeDelta.x) * 2;
+            position.y = (position.y / background.sizeDelta.y) * 2;
+
+            InputVector = new Vector2(position.x, position.y);
             
-            InputVector = pos / radius;
+            if (InputVector.magnitude > 1)
+            {
+                InputVector = InputVector.normalized;
+            }
+
+            handle.anchoredPosition = new Vector2(
+                InputVector.x * (background.sizeDelta.x / 2) * handleRange, 
+                InputVector.y * (background.sizeDelta.y / 2) * handleRange
+            );
         }
     }
-
-    public void OnPointerDown(PointerEventData eventData) => OnDrag(eventData);
 
     public void OnPointerUp(PointerEventData eventData)
     {
